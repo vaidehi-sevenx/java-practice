66be5607de7e747f1814ce6676a840ecd52dbf08
vaidehi-sevenx <98866576+vaidehi-sevenx@users.noreply.github.com>
2022-02-04 16:18:47 +0530

    Add files via upload

---
+++ javaPractice/factorial.java
@@ -0,0 +1,14 @@
+/* java t point*/
+/*problem statement:factorial program using loop */
+package javaPractice;
+
+public class factorial{
+	public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=6;
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+		}  
